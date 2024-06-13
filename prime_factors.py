d9aa45032beb8d4aa24ffa105e73a091127040cd
ymin86kim <ymin86kim@gmail.com>
2024-06-13 09:36:26 +0900

    [feature] 31

--- prime_factors.py
+++ prime_factors.py
@@ -3,4 +3,6 @@ class PrimeFactor:
         factors = []
         if number == 2:
             factors.append(2)
+        elif number == 3:
+            factors.append(3)
         return factors
--- test_prime_factors.py
+++ test_prime_factors.py
@@ -13,3 +13,6 @@ class PrimeFactorTest(TestCase):
 
     def test_prime_factor_of_2(self):
         self.assertEqual([2], self.prime_factor.of(2))
+
+    def test_prime_factor_of_3(self):
+        self.assertEqual([3], self.prime_factor.of(3))
