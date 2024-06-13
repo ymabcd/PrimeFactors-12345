8e2649d2671dc65dddcda8c3daf95fe3a84d5b1f
ymin86kim <ymin86kim@gmail.com>
2024-06-13 09:48:20 +0900

    [feature] 45

--- prime_factors.py
+++ prime_factors.py
@@ -14,6 +14,9 @@ class PrimeFactor:
                         factors.append(divisor)
                         number //= divisor
                     divisor += 1
+            elif number == 9:
+                factors.append(3)
+                factors.append(3)
             else:
                 factors.append(number)
         return factors
--- test_prime_factors.py
+++ test_prime_factors.py
@@ -22,3 +22,6 @@ class PrimeFactorTest(TestCase):
 
     def test_prime_factor_of_6(self):
         self.assertEqual([2, 3], self.prime_factor.of(6))
+
+    def test_prime_factor_of_9(self):
+        self.assertEqual([3, 3], self.prime_factor.of(9))
