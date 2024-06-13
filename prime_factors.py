f684e8efbc5991d6533b9b9c78b61e97d037b248
ymin86kim <ymin86kim@gmail.com>
2024-06-13 09:42:23 +0900

    [refactoring] 37

--- prime_factors.py
+++ prime_factors.py
@@ -3,8 +3,9 @@ class PrimeFactor:
         factors = []
         if number > 1:
             if number == 4:
-                factors.append(2)
-                factors.append(2)
+                while number % 2 == 0:
+                    factors.append(2)
+                    number //= 2
             else:
                 factors.append(number)
         return factors
